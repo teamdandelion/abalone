613df41448f766328054bbf07fe47c0bc14280f7
Daniel Mané <danmane@gmail.com>
2015-01-05 20:34:05 +0200

    Implement most of `update`. Game engine nearly done! Almost compiles

--- abalone.hs
+++ abalone.hs
@@ -1,17 +1,17 @@
 module Abalone (
-	AbaloneGame, AbaloneBoard, Player(White, Black), Position,
+	Game, Board, Player(White, Black), Position,
 	gameOver, winner, numPieces) where
 
 import qualified Data.Set as Set
 
-data AbaloneBoard = AbaloneBoard {
+data Board = Board {
 	whitePositions :: Set.Set Position,
 	blackPositions :: Set.Set Position,
 	boardRadius    :: Int
 } deriving (Eq, Show, Read)
 
-data AbaloneGame = AbaloneGame {
-	board 		   :: AbaloneBoard,
+data Game = Game {
+	board 		   :: Board,
 	nextPlayer 	   :: Player,
 	movesRemaining :: Int,
 	marblesPerMove :: Int
@@ -26,7 +26,7 @@ data Move = Move {
 } deriving (Eq, Show, Read)
 
 data Segment = Segment {
-	head        :: Position,
+	basePos     :: Position,
 	orientation :: Direction,
 	segLength   :: Int
 } deriving (Eq, Show, Read)
@@ -34,7 +34,7 @@ data Segment = Segment {
 data Direction = TopRight | MidRight | BotRight 
 			   | BotLeft  | MidLeft  | TopLeft deriving (Eq, Show, Read, Ord, Bounded)
 
-getPieces :: AbaloneBoard -> Player -> Set.Set Position
+getPieces :: Board -> Player -> Set.Set Position
 getPieces b p = if p == White then whitePositions else blackPositions $ b
 
 adjacent :: Direction -> Position -> Position
@@ -55,10 +55,10 @@ next p = case p of
 	Black -> White
 
 
-gameOver :: AbaloneGame -> Bool
+gameOver :: Game -> Bool
 gameOver g = movesRemaining g <= 0 || any (\p -> numPieces g p == 0) [White, Black]
 
-winner :: AbaloneGame -> Maybe Player
+winner :: Game -> Maybe Player
 winner g = if gameOver g then advantage else Nothing where
 	w = numPieces g White
 	b = numPieces g Black
@@ -67,32 +67,66 @@ winner g = if gameOver g then advantage else Nothing where
 		| w < b     = Just Black
 		| otherwise	= Nothing
 
-numPieces :: AbaloneGame -> Player -> Int
+numPieces :: Game -> Player -> Int
 numPieces g p 
 	| p == White = Set.size . whitePositions $ g
 	| p == Black = Set.size . blackPositions $ g 
 
-isValid :: AbaloneGame -> AbaloneGame -> Bool
+isValid :: Game -> Game -> Bool
 -- very inefficient impl but that should be fine
 isValid g0 g1 = g1 `elem` map (update g0) (possibleMoves g0)
 
-update :: AbaloneGame -> Move -> AbaloneGame
+colinear :: Direction -> Direction -> Bool
+colinear d1 d2 = d1 `elem` [d2, opposite d2]
 
-possibleMoves :: AbaloneGame -> [Move]
+update :: Game -> Move -> Game
+update (Game b p remaining perMove) (Move (Segment pos orient len) dir) = newGame where
+	newGame = Game newBoard (next p) (remaining - 1) perMove
+	ownPieces = take len $ iterate (adjacent orient) pos
+	enemyPieces = if colinear orient dir then recur start (len-1) else [] where 
+		start = adjacent dir $ if orient == dir then last ownPieces else pos
+		enemy = \x -> x `Set.member` getPieces b (next p)
+		recur x force 
+			| not . enemy x = []
+			| force == 0    = []
+			| otherwise     = x : recur (adjacent dir x) (force - 1)
+
+	radius = boardRadius b
+	onBoard = 
+	updated = filter onBoard . map (adjacent dir)
+
+	whiteMoved = if p == White then ownPieces else enemyPieces
+	blackMoved = if p == Black then ownPieces else enemyPieces
+
+	removeAndAdd :: Set.Set a -> [a] -> [a] -> Set.Set a
+	removeAndAdd s toRemove toAdd = Set.union (Set.fromList toAdd) removed where
+		removed = Set.difference s (Set.fromList toRemove)
+
+	white = removeAndAdd (whitePositions b) whiteMoved (updated whiteMoved)
+	black = removeAndAdd (blackPositions b) blackMoved (updated blackMoved)
+
+	newBoard = Board white black (boardRadius b)
+
+
+possibleMoves :: Game -> [Move]
 possibleMoves g = filter isMove allMoves where
 	allMoves = concat $ map (map Move (segments g)) [TopRight .. TopLeft]
 	b = board g
 	own   p = Set.member p $ getPieces b $ nextPlayer g
 	enemy p = Set.member p $ getPieces b $ next $ nextPlayer g
 	free  p = (not . own) p && (not . enemy) p
-	isMove (Move (Segment pos orient len) dir) = if aligned then forward else broadside where 
-		aligned = dir `elem` [orient, opposite orient] where 
+	isMove m = if colinear dir orient then forward else broadside where 
+		dir     = direction m
+		orient  = orientation . segment m
+		len     = segLength   . segment m
+		pos     = basePos        . segment m
+		aligned = dir `elem` [orient, opposite orient] 
 
 		broadside = all free destination where
 			destination = take len $ iterate (adjacent orient) $ adjacent dir pos
 
 		forward = iterF (adjacent dir front) (len - 1) where
-			front = if (orient == dir) then iterate (adjacent dir) pos !! len - 1 else pos
+			front = if (orient == dir) then iterate (adjacent dir) pos !! len-1 else pos
 			iterF pos force 
 				| free pos   = True
 				| own  pos   = False
@@ -103,9 +137,9 @@ possibleMoves g = filter isMove allMoves where
 cross :: [a] -> [b] -> [(a, b)]
 cross as bs = concat $ map (\a -> map (\b -> (a, b)) bs) as 
 
-segments :: AbaloneGame -> [Segment]
+segments :: Game -> [Segment]
 segments g = segments_ (marblesPerMove g) (nextPlayer g) (board g) where
-	segments_ :: Int -> AbaloneBoard -> Player -> [Segment]
+	segments_ :: Int -> Board -> Player -> [Segment]
 	segments_ len b p = concat $ map f positionDirectionPairs where
 		pieces = getPieces b p 
 		positionDirectionPairs = cross (Set.toList pieces) [TopRight, MidRight, BotRight]
