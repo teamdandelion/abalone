b20285576cb4e07540deb4cf351c0dcba19a1d1f
Daniel Mané <danmane@gmail.com>
2015-01-05 07:24:36 +0200

    Improve data structure

--- abalone.hs
+++ abalone.hs
@@ -1,38 +1,48 @@
+
+module Abalone (
+	AbaloneGame, AbaloneRules, Player(White, Black), Position,
+	gameOver, winner, numPieces) where
+
+import qualified Data.Set as Set
+
+data AbaloneBoard = AbaloneBoard {
+	whitePositions :: Set.Set Position,
+	blackPositions :: Set.Set Position,
+	boardRadius    :: Int
+}
+
 data AbaloneGame = AbaloneGame {
-	whitePositions :: [Position],
-	blackPositions :: [Position],
+	board 		   :: AbaloneBoard,
 	nextPlayer 	   :: Player,
-	turnCounter    :: Int,
-	rules          :: AbaloneRules
-} deriving (Show)
+	movesRemaining :: Int,
+	marblesPerMove :: Int,
+} deriving (Eq, Show, Read)
 
-data AbaloneRules = AbaloneRules {
-	boardRadius :: Int,
-	maxMarblesPerMove :: Int,
-	movesUntilEnd :: Int
-} deriving (Show)
-
-data Player = White | Black deriving (Eq, Show)
+data Player = White | Black deriving (Eq, Show, Read, Ord, Bounded)
 type Position = (Int, Int)
 
 gameOver :: AbaloneGame -> Bool
-gameOver g = turnCounter g >= rules . movesUntilEnd g 
-				|| length whitePositions g == 0 
-				|| length blackPositions g == 0
+gameOver g = movesRemaining g <= 0 || any (\p -> numPieces g p == 0) [White, Black]
 
 winner :: AbaloneGame -> Maybe Player
-winner g 
-	| gameOver g = advantage g
-	| otherwise  = Nothing
-	where
-	advantage :: AbaloneGame -> Maybe Player 
-	advantage g
-		| numPieces White > numPieces Black = Just White
-		| numPieces White < numPieces Black = Just Black
-		| otherwise							= Nothing
-
+winner g = if gameOver g then advantage else Nothing where
+	w = numPieces g White
+	b = numPieces g Black
+	advantage 
+		| w > b     = Just White
+		| w < b     = Just Black
+		| otherwise	= Nothing
 
 numPieces :: AbaloneGame -> Player -> Int
 numPieces g p 
-	| p == White = length . whitePositions g
-	| p == Black = length . blackPositions g 
+	| p == White = Set.size . whitePositions $ g
+	| p == Black = Set.size . blackPositions $ g 
+
+isValid :: AbaloneGame -> AbaloneGame -> Bool
+isValid g0 g1 
+	| marblesPerMove g0 /= marblesPerMove g1 = False
+	| nextPlayer g0 == nextPlayer g1 = False
+	| movesRemaining g0 /= movesRemaining g1 + 1 = False
+	| otherwise = isValid (board g0) (board g1)
+
+isValid :: AbaloneBoard -> AbaloneBoard -> Bool
