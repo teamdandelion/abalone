fd4b72cbafec72d9a07853ad5eecb90e2043b8a8
Daniel Mané <danmane@gmail.com>
2015-01-05 10:12:01 +0200

    Work towrads enumerating all moves: Move datatype Segment datatype fn to enumerate all segments

--- abalone.hs
+++ abalone.hs
@@ -1,6 +1,5 @@
-
 module Abalone (
-	AbaloneGame, AbaloneRules, Player(White, Black), Position,
+	AbaloneGame, AbaloneBoard, Player(White, Black), Position,
 	gameOver, winner, numPieces) where
 
 import qualified Data.Set as Set
@@ -9,7 +8,7 @@ data AbaloneBoard = AbaloneBoard {
 	whitePositions :: Set.Set Position,
 	blackPositions :: Set.Set Position,
 	boardRadius    :: Int
-}
+} deriving (Eq, Show, Read)
 
 data AbaloneGame = AbaloneGame {
 	board 		   :: AbaloneBoard,
@@ -20,6 +19,48 @@ data AbaloneGame = AbaloneGame {
 
 data Player = White | Black deriving (Eq, Show, Read, Ord, Bounded)
 type Position = (Int, Int)
+
+data Move = Move {
+	segment   :: Segment,
+	direction :: Direction,
+} deriving (Eq, Show, Read)
+
+data Segment = Segment {
+	head        :: Position,
+	orientation :: Direction,
+	length      :: Int,
+} deriving (Eq, Show, Read)
+
+data Direction = TopRight | MidRight | BotRight 
+			   | BotLeft  | MidLeft  | TopLeft deriving (Eq, Show, Read, Ord, Bounded)
+
+getPieces :: AbaloneBoard -> Player -> Set.Set Position
+getPieces b p = if p == White then whitePositions else blackPositions $ b
+
+segments :: AbaloneGame -> [Segment]
+segments g = segments (marblesPerMove g) (nextPlayer g) (board g)
+
+segments :: Int -> AbaloneBoard -> Player -> [Segment]
+segments len b p = flatten $ map f positionDirectionPairs where
+	pieces = getPieces b p 
+	positionDirectionPairs = zip (toList pieces) [TopRight, MidRight, BotRight]
+	f :: (Position, Direction) -> [Segment]
+	f (p, d) = map (Segment p d) [1..numSegments] where
+		tails = iterate (adjacent (opposite d)) p
+		validTails = takeWhile (\p -> p `member` pieces) tails
+		numSegments = min (length validTails) len
+
+adjacent :: Direction -> Position -> Position
+
+opposite :: Direction -> Direction
+opposite d = case d of 
+	TopRight -> BotLeft
+	MidRight -> MidLeft
+	BotRight -> TopLeft
+	BotLeft  -> TopRight
+	MidLeft  -> MidRight
+	TopLeft  -> BotRight
+
 
 gameOver :: AbaloneGame -> Bool
 gameOver g = movesRemaining g <= 0 || any (\p -> numPieces g p == 0) [White, Black]
@@ -39,10 +80,9 @@ numPieces g p
 	| p == Black = Set.size . blackPositions $ g 
 
 isValid :: AbaloneGame -> AbaloneGame -> Bool
-isValid g0 g1 
-	| marblesPerMove g0 /= marblesPerMove g1 = False
-	| nextPlayer g0 == nextPlayer g1 = False
-	| movesRemaining g0 /= movesRemaining g1 + 1 = False
-	| otherwise = isValid (board g0) (board g1)
+-- very inefficient impl but that should be fine
+isValid g0 g1 = g1 `elem` map (update g0) (possibleMoves g0)
 
-isValid :: AbaloneBoard -> AbaloneBoard -> Bool
+update :: AbaloneGame -> Move -> AbaloneGame
+
+possibleMoves :: AbaloneGame -> [Move]
