b6ca10fc80f0622301264b723ff0b9c86220a177
Daniel Mané <danmane@gmail.com>
2015-01-07 15:36:46 +0200

    Apply structuralist's cleanup to player.hs

--- src/player.hs
+++ src/player.hs
@@ -2,6 +2,5 @@ module Player (Player(White, Black), next) where
 -- Player & Related Functions--
 data Player = White | Black deriving (Eq, Show, Read, Ord, Bounded, Enum)
 next :: Player -> Player
-next p = case p of
-	White -> Black
-	Black -> White
+next White = Black
+next Black = White
