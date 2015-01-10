d9ad469536460d33ae855aaf40c9b2002eae8c5c
Daniel Mané <danmane@gmail.com>
2015-01-10 11:18:28 +0200

    Finish converting the abalone implementation over to elm. need to handle JSON conversion

--- elm/model.elm
+++ elm/model.elm
@@ -103,15 +103,15 @@ numPieces : Game -> Player -> Int
 numPieces g p = List.length <| Set.toList <| getPieces g.board p
 
 -- this function will recieve new game states from client and verify validity
---isValid : Game -> Game -> Bool
---isValid g0 g1 = List.member g1 (futures g0) -- very inefficient impl but that should be fine since occurs once per turn
+isValid : Game -> Game -> Bool
+isValid g0 g1 = List.member g1 (futures g0) -- very inefficient impl but that should be fine since occurs once per turn
 
 onBoard : Board -> Position -> Bool -- is a piece on the board still?
 onBoard board pos = dist2 pos (0, 0) <= board.boardRadius * 2
 
 -- ===
---futures : Game -> List Game -- find all valid future states of this board (1 move)
---futures g = map (update g) (possibleMoves g)
+futures : Game -> List Game -- find all valid future states of this board (1 move)
+futures g = map (update g) (possibleMoves g)
 
 crossApply : List (a -> b) -> List a -> List b
 crossApply fs xs = List.foldr (\a  -> (++) <| (flip List.map) xs a) [] fs
@@ -152,6 +152,23 @@ inlineMoved b m = if broadside m
                  | otherwise -> Maybe.map ((::) x) <| clear (adjacent m.direction x) (force - 1)
          in clear attacked (m.segment.segLength - 1)
 
+fromJust : Maybe a -> a
+fromJust (Just x) = x
+
+update : Game -> Move -> Game
+update {board, nextPlayer, movesRemaining, marblesPerMove} m = let 
+    ownPieces = segPieces m.segment
+    enemyPieces = if broadside m then [] else fromJust <| inlineMoved board m
+    updated = List.filter (onBoard board) << map (adjacent m.direction)
+    whiteMoved = if nextPlayer == White then ownPieces else enemyPieces
+    blackMoved = if nextPlayer == Black then ownPieces else enemyPieces
+    diff  s l = Set.diff  s (Set.fromList l)
+    union s l = Set.union s (Set.fromList l)
+    newWhitePos = (board.whitePositions `diff` whiteMoved) `union` updated whiteMoved
+    newBlackPos = (board.blackPositions `diff` blackMoved) `union` updated blackMoved
+    newBoard = {whitePositions = newWhitePos, blackPositions = newBlackPos, boardRadius = board.boardRadius}
+    newGame = {board = newBoard, nextPlayer = next nextPlayer, movesRemaining = movesRemaining - 1, marblesPerMove = marblesPerMove}
+                                                               in newGame
 
 last : List a -> a
 last = List.reverse >> List.head
