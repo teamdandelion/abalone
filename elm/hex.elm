1a43c5450e05c1888a0e156999a713bcd7a05a99
Daniel Mané <danmane@gmail.com>
2015-01-11 19:41:03 +0200

    [progress] add helper functions to hex.elm: nearbyDirections (the adjacent 2 directions to chosen direction) findDirection (if two positions are colinear, find the direction from 1 to the other)

--- elm/hex.elm
+++ elm/hex.elm
@@ -1,5 +1,7 @@
 module Hex where
 import List
+import Dict
+import Misc
 type Direction = TopRight | MidRight | BotRight | TopLeft | MidLeft | BotLeft
 
 type alias Position = (Int, Int)
@@ -25,13 +27,19 @@ opposite d = case d of
     MidLeft  -> MidRight
     TopLeft  -> BotRight
 
+directions = [TopRight, MidRight, BotRight, BotLeft, MidLeft, TopLeft]
+
+nearbyDirections : Direction -> List Direction
+nearbyDirections d = 
+    let idx = Misc.fromJust <| Misc.index d directions
+        nearby = map (\x -> (x + idx) % 6) [-1, 0, 1]
+    in  List.map (Misc.fromJust << (flip Misc.retrieve <| directions)) nearby
+
 colinear : Direction -> Direction -> Bool
 colinear d1 d2 = d1 == d2 || d1 == opposite d2
 
 hexagonalGrid : Int -> List Position
 hexagonalGrid rad = List.concat <| List.map ring [0..rad - 1]
-
-directions = [TopRight, MidRight, BotRight, BotLeft, MidLeft, TopLeft]
 
 
 tailOrOnly : List a -> List a
@@ -40,3 +48,12 @@ tailOrOnly (x::xs) = if xs == [] then [x] else xs
 ring : Int -> List Position
 ring n = let ds = List.concat <| List.map (List.repeat n << adjacent) directions 
          in tailOrOnly <| List.scanl identity (-n, 0) ds 
+
+findDirection : Position -> Position -> Maybe Direction
+findDirection (q1, r1) (q2, r2) = 
+    if
+        | q1 == q2 && r1 == r2 -> Nothing
+        | q1 == q2             -> if r1 < r2 then Just BotRight else Just TopLeft
+        | r1 == r2             -> if q1 < q2 then Just MidRight else Just MidLeft
+        | r1 + q1 == r2 + q2   -> if q1 < q2 then Just TopRight else Just BotLeft
+        | otherwise            -> Nothing
