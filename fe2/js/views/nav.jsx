99bf09e31b5f7d4aaa6c64f76e5b4b071938ad3c
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-02-13 00:08:21 -0800

    add basic skeleton of upload page

--- fe2/js/views/nav.jsx
+++ fe2/js/views/nav.jsx
@@ -7,7 +7,9 @@ module.exports = React.createClass({
   render: function() {
     return (
       <Nav bsStyle="tabs" activeKey={this.props.activeKey}  style={{"margin-bottom": "60px"}}>
-        <NavItem key={1} href="/"><i className="fa fa-desktop"></i> Home</NavItem>
+        <NavItem key={1} href="/"><i className="fa fa-trophy"></i> Leaderboard</NavItem>
+        <NavItem key={2} href="/play"><i className="fa fa-gamepad"></i> Play</NavItem>
+        <NavItem key={3} href="/upload"><i className="fa fa-upload"></i> Upload a Player</NavItem>
       </Nav>
     )
   }
