3b27e6335b99bc420feeee8e67ab1022bdba9eee
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-02-16 16:28:35 -0800

    add JSON Marshal/Unmarshal methods on HexSet
    fix HexSet UnmarshalJSON

--- go/abalone/hexSet.go
+++ go/abalone/hexSet.go
@@ -1,6 +1,28 @@
 package abalone
 
+import "encoding/json"
+
 type HexSet map[Hex]struct{}
+
+func (s HexSet) MarshalJSON() ([]byte, error) {
+	var out []Hex
+	for h, _ := range s {
+		out = append(out, h)
+	}
+	return json.Marshal(out)
+}
+
+func (s *HexSet) UnmarshalJSON(data []byte) error {
+	*s = make(HexSet)
+	var in []Hex
+	if err := json.Unmarshal(data, &in); err != nil {
+		return err
+	}
+	for _, h := range in {
+		(*s)[h] = struct{}{}
+	}
+	return nil
+}
 
 func (hm *HexSet) hexSet2Slice() []Hex {
 	res := make([]Hex, len(*hm))
