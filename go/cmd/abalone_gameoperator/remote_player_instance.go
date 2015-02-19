04bba44e35d089e2c2a9a2a5ce49fcdffbbf9313
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-02-19 12:31:47 -0800

    extract RemotePlayerInstance

---
+++ go/cmd/abalone_gameoperator/remote_player_instance.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/cenkalti/backoff"
+	"github.com/danmane/abalone/go/api"
+	"github.com/danmane/abalone/go/game"
+)
+
+type RemotePlayerInstance struct {
+	APIPlayer api.Player
+	Port      string
+}
+
+func (i *RemotePlayerInstance) Player() api.Player {
+	return i.APIPlayer
+}
+
+func (i *RemotePlayerInstance) Play(s *game.State, limit time.Duration) (*game.State, error) {
+	return gameFromAI(i.Port, s)
+}
+
+func gameFromAI(port string, state *game.State) (*game.State, error) {
+	mr := api.MoveRequest{
+		State:      *state,
+		LimitMilli: toMillisecondCount(api.DefaultMoveLimit),
+	}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(&mr); err != nil {
+		return nil, err
+	}
+
+	var rawResponse bytes.Buffer // will contain response if HTTP request is successful
+	f := func() error {
+		resp, err := http.Post("http://localhost:"+port+api.MovePath, "application/json", bytes.NewBuffer(buf.Bytes()))
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		io.Copy(&rawResponse, resp.Body)
+		return nil
+	}
+
+	if err := withRetries(f); err != nil {
+		return nil, err
+	}
+
+	responseGame := &game.State{}
+	if err := json.NewDecoder(&rawResponse).Decode(responseGame); err != nil {
+		return nil, err
+	}
+	if !state.ValidFuture(responseGame) {
+		return nil, fmt.Errorf("game parsed correctly, but isn't a valid future")
+	}
+	return responseGame, nil
+}
+
+func withRetries(f func() error) error {
+	backoffConfig := backoff.NewExponentialBackOff()
+	backoffConfig.InitialInterval = time.Second
+	backoffConfig.MaxInterval = 10 * time.Second
+	backoffConfig.MaxElapsedTime = 60 * time.Second
+
+	notifyFunc := func(err error, dur time.Duration) {
+		log.Printf("waiting %v, failed to get move from player: %s", dur, err)
+	}
+	err := backoff.RetryNotify(f, backoffConfig, notifyFunc)
+	return err
+}
