2441c1bd5e80e9f3c223a44ec08f0e6d33eb046d
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-02-21 15:39:41 -0800

    feat(ctl) add users subcommands

--- go/cmd/abalone_ctl/main.go
+++ go/cmd/abalone_ctl/main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"flag"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/codegangsta/cli"
-	"github.com/danmane/abalone/go/router"
 )
 
 const (
 	DefaultHTTPDHost = "localhost:8080" // TODO extract
 )
 
-var (
-	httpdHost = flag.String("httpd", DefaultHTTPDHost, "abalone_httpd listening addr:port")
-)
-
 func main() {
-	flag.Parse()
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
@@ -33,50 +22,15 @@ func run() error {
 	app := cli.NewApp()
 	app.Name = "abalone_ctl"
 	app.Usage = "abalone management util"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "httpd",
+			Value: DefaultHTTPDHost,
+			Usage: "abalone_httpd listening addr:port",
+		},
+	}
 	app.Commands = []cli.Command{
-		RunCmd,
+		UsersCmd,
 	}
 	return app.Run(os.Args)
-}
-
-var RunCmd = cli.Command{
-	Name:   "run",
-	Usage:  "runs a game on abalone_httpd",
-	Action: RunGameAction,
-}
-
-func RunGameAction(c *cli.Context) {
-	if err := doRunGame(c); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func doRunGame(c *cli.Context) error {
-	req := struct {
-		BlackPort string
-		WhitePort string
-	}{
-		BlackPort: "3423",
-		WhitePort: "3424",
-	}
-
-	r := router.NewAPIRouter()
-	path, err := r.Get(router.GamesRun).URL()
-	if err != nil {
-		return err
-	}
-	url := fmt.Sprintf("http://%s%s", *httpdHost, path.String())
-
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(&req); err != nil {
-		return err
-	}
-	resp, err := http.Post(url, "application/json", &buf)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != http.StatusOK {
-		log.Println(resp.Status)
-	}
-	return nil
 }
---
+++ go/cmd/abalone_ctl/users.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/codegangsta/cli"
+	"github.com/danmane/abalone/go/api"
+	"github.com/danmane/abalone/go/router"
+	"github.com/olekukonko/tablewriter"
+)
+
+var UsersCmd = cli.Command{
+	Name:  "users",
+	Usage: "manage abalone users",
+	Subcommands: []cli.Command{
+		{
+			Name:  "create",
+			Usage: "creates a user",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "name, n",
+					Value: "",
+				},
+				cli.StringFlag{
+					Name:  "email, e",
+					Value: "",
+				},
+			},
+			Action: func(c *cli.Context) {
+				if err := CreateUsersHandler(c); err != nil {
+					log.Fatal(err)
+				}
+			},
+		},
+		{
+			Name:  "list",
+			Usage: "lists users",
+			Action: func(c *cli.Context) {
+				if err := ListUsersHandler(c); err != nil {
+					log.Fatal(err)
+				}
+			},
+		},
+	},
+}
+
+func CreateUsersHandler(c *cli.Context) error {
+	r := router.NewAPIRouter()
+	path, err := r.Get(router.UsersCreate).URL()
+	if err != nil {
+		return err
+	}
+
+	if !c.IsSet("name") {
+		return errors.New("name is required")
+	}
+	n := c.String("name")
+
+	if !c.IsSet("email") {
+		return errors.New("email is required")
+	}
+	e := c.String("email")
+
+	req := api.User{
+		Name:  n,
+		Email: e,
+	}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(&req); err != nil {
+		return err
+	}
+	url := fmt.Sprintf("http://%s%s", c.GlobalString("httpd"), path.String())
+	resp, err := http.Post(url, "application/json", &buf)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error: %s", resp.Status)
+	}
+	return nil
+}
+
+func ListUsersHandler(c *cli.Context) error {
+	r := router.NewAPIRouter()
+	path, err := r.Get(router.Users).URL()
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("http://%s%s", c.GlobalString("httpd"), path.String())
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error: %s", resp.Status)
+	}
+	var users []api.User
+	if err := json.NewDecoder(resp.Body).Decode(&users); err != nil {
+		return fmt.Errorf("error decoding json response: %s", err)
+	}
+
+	table := tablewriter.NewWriter(c.App.Writer)
+	table.SetHeader([]string{"ID", "Name", "Email", "Created", "Updated"})
+	for _, u := range users {
+		row := []string{
+			strconv.FormatInt(u.ID, 10),
+			u.Name,
+			u.Email,
+			u.CreatedAt.String(),
+			u.UpdatedAt.String(),
+		}
+		table.Append(row)
+	}
+	table.Render() // Send output
+	return nil
+}
