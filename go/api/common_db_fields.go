ea6b32604ead379b25ba75c6fa26710ce12c52ba
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-02-21 16:02:29 -0800

    refactor(api) move ID field off of CommonDBFields
    can't use ID in struct literal when it's embedded. put it on the struct to reduce friction

--- go/api/common_db_fields.go
+++ go/api/common_db_fields.go
@@ -3,7 +3,6 @@ package api
 import "time"
 
 type CommonDBFields struct {
-	ID        int64 `gorm:"column:id"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt time.Time
--- go/api/user.go
+++ go/api/user.go
@@ -4,5 +4,7 @@ type User struct {
 	Name  string
 	Email string
 
+	ID int64 `gorm:"column:id"`
+
 	CommonDBFields
 }
