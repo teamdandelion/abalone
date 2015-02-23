82496178306bb855e0c8d300611a783bab44115f
Daniel Mané <danmane@gmail.com>
2015-02-23 01:01:03 -0800

    Factor example AI to be more clear

--- go/cmd/abalone_example_ai/main.go
+++ go/cmd/abalone_example_ai/main.go
@@ -12,13 +12,15 @@ var (
 	port = flag.String("port", "3423", "port the ai runs on")
 )
 
+func play(s game.State, limit time.Duration) game.State {
+	f := s.Futures()
+	if len(f) == 0 {
+		panic("Ah! There are no future states. Why'd the server send this to me? =( ")
+	}
+	return *f[0]
+}
+
 func main() {
 	flag.Parse()
-	quickstart.Run(*port, func(s game.State, limit time.Duration) game.State {
-		f := s.Futures()
-		if len(f) == 0 {
-			panic("Ah! There are no future states. Why'd the server send this to me? =( ")
-		}
-		return *f[0]
-	})
+	quickstart.Run(*port, play)
 }
