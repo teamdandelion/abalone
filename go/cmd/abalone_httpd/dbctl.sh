bbc375740adb0320e4e05d86bf9119f2c834f91e
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-02-24 03:10:37 -0800

    add comment to sleep

--- go/cmd/abalone_httpd/dbctl.sh
+++ go/cmd/abalone_httpd/dbctl.sh
@@ -25,6 +25,12 @@ then
         docker run --rm --link $name:$name -e "PGPASSWORD=$dbpass" $img $@
     }
 
+    # we expect the server to take a few seconds to boot up. the following
+    # while loop does the right thing by waiting until the server is reachable,
+    # so this sleep statement is not strictly necessary. however, the errors
+    # that are printed to stdout during the first couple seconds of waiting are
+    # distracting to the user. the sole purpose of this sleep statement is to
+    # avoid spewing unactionable noise to the console in the interim.
     sleep 3
 
     while ! with_connection_to_postgresql_server psql -c \\conninfo -h $name -U $dbuser | grep "You are connected"
